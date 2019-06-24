a0a0f2baad4a75e09b3a9bda132d3472f84907b0
dDamyanov0305 <dimitar.damyanov.0305@gmail.com>
2019-06-24 19:57:34 +0300

    unsaved work

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require('passport');
 const session = require("express-session");
-const generalRouter = express.Router();
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -20,16 +19,13 @@ mongoose
     .catch((err)=>{console.log(err)});
 
 
-//enable body parsing
-//app.use(express.urlencoded({ extended:false }));
-
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/user', require('./routes/users.js'));
-app.use('/car', require('./routes/cars.js'));
-app.use('/', (req,res) => res.send("henlo"));
+app.use('/user', require('./routes/user.js'));
+//app.use('/car', require('./routes/cars.js'));
+app.get('/', (req,res) =>{ res.send('root'); });
 
 const PORT = process.env.PORT||5000;
 
